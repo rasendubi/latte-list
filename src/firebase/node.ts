84dcb7dc41b08e7c2e6e500596738400203b47ce
Alexey Shmalko <rasen.dubi@gmail.com>
2021-09-06 19:32:08 +0300

    refactor: drop NEXT_PUBLIC_ prefix from old env vars

--- src/firebase/node.ts
+++ src/firebase/node.ts
@@ -3,11 +3,11 @@ import * as admin from 'firebase-admin';
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
 }
 
